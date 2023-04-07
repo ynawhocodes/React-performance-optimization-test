9bdb669f4f557ba6281a69b5e2be0655c9760c5a
ynawhocodes <ynawhocodes@gmail.com>
2023-04-07 16:42:42 +0900

    feat: create TodoList, TodoListItem components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
