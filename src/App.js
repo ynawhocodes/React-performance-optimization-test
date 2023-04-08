5f819a3e3559fc4aefd2e624f633c8e6b75d0c5f
ynawhocodes <ynawhocodes@gmail.com>
2023-04-08 20:28:50 +0900

    feat: insert todo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import React, { useState, useRef, useCallback } from 'react';
+const dummyTodos = [
+  { id: 0, text: 'todo1', checked: true },
+  { id: 1, text: 'todo2', checked: false },
+  { id: 2, text: 'todo3', checked: false },
+];
 function App() {
-  const dummyTodos = [
-    { id: 1, text: 'todo1', checked: true },
-    { id: 2, text: 'todo2', checked: false },
-    { id: 3, text: 'todo3', checked: false },
-  ];
+  const [todos, setTodos] = useState(dummyTodos);
+  const nextId = useRef(3);
+
+  const onInsert = useCallback(
+    (newTodoText) => {
+      const newTodo = {
+        id: nextId.current,
+        text: newTodoText,
+        checked: false,
+      };
+      setTodos(todos.concat(newTodo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+  console.log(todos);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos={dummyTodos} />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [todoText, setTodoText] = useState('');
+  const onChange = useCallback((e) => {
+    setTodoText(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(todoText);
+      setTodoText('');
+      e.preventDefault();
+    },
+    [onInsert, todoText],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="Write your DUMMY TODO" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="Write your DUMMY TODO"
+        value={todoText}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
