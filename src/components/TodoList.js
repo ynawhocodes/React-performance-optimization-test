3f9b0c7078bb0503eb844c9a84e33b97a09acaa3
yanwhocodes <ynawhocodes@gmail.com>
2023-04-11 14:59:52 +0900

    perf: use react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
-          key={todo.id}
+          key={key}
           item={todo}
           onRemove={onRemove}
           onToggle={onToggle}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+    />
   );
 };
 
